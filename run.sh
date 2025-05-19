65a771590c2a81c960b698bcbf248da89c98b2d5
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-05-19 20:34:27 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,10 +7,24 @@ wget https://github.com/JeversonDiasSilva/retroplay/releases/download/v1.0/run.j
 if [ -f "run.jc" ]; then
     # Dá permissão de execução ao arquivo
     chmod +x run.jc
-
-    # Executa o arquivo
-    ./run.jc
 else
     echo "Erro: O arquivo run.jc não foi baixado corretamente."
+    exit 1
+fi
+
+# Interação para a chave de instalação
+URL="https://raw.githubusercontent.com/JeversonDiasSilva/data/main/db.txt"
+LISTA=$(curl -s "$URL")
+
+echo -e "\nDigite a chave de instalação para continuar:"
+read -p ":: " CHAVE
+
+# Verifica se a chave é válida
+if echo "$LISTA" | grep -qx "$CHAVE"; then
+    echo -e "✔ Chave válida. Continuando a instalação..."
+    ./run.jc  # Executa o script run.jc após chave válida
+else
+    echo -e "✘ Chave inválida."
+    echo -e "Procure suporte técnico: Retrô LuXXo - WHATS (41) 99820-5080"
     exit 1
 fi
