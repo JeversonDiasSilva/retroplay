89d3c1c83de03ce2a331929daa2a55888320e64d
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-05-19 20:43:35 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-# Baixa o arquivo run.jc da URL fornecida
+# Passo 1: Baixar o arquivo do GitHub
+echo "Baixando o arquivo..."
 wget https://github.com/JeversonDiasSilva/retroplay/releases/download/v1.0/run.jc -O run.jc
 
-# Verifica se o arquivo foi baixado corretamente
-if [ -f "run.jc" ]; then
-    # Dá permissão de execução ao arquivo
-    chmod +x run.jc
-else
-    echo "Erro: O arquivo run.jc não foi baixado corretamente."
-    exit 1
-fi
+# Passo 2: Exibir a mensagem e aguardar o usuário pressionar Enter para prosseguir
+echo "Pressione ENTER para prosseguir"
+read -p ""  # Espera o usuário pressionar Enter
 
-# Avisa o usuário que o script está pronto para rodar
-echo -e "\nO arquivo run.jc foi baixado e está pronto para execução."
-
-# Solicita ao usuário pressionar Enter para continuar
-echo -e "Pressione Enter para executar o script run.jc..."
-read  # Espera o pressionamento da tecla Enter
-
-# Executa o arquivo run.jc
+# Passo 3: Rodar o comando ./run.jc
+echo "Executando o comando ./run.jc..."
+chmod +x run.jc  # Garantir permissão de execução
 ./run.jc
