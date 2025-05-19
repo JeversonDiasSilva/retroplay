b094c30ccf34e5e6b99165f47cd0fd8f3ff30637
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-05-19 20:45:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 
 # Passo 1: Baixar o arquivo do GitHub
 echo "Baixando o arquivo..."
-wget https://github.com/JeversonDiasSilva/retroplay/releases/download/v1.0/run.jc -O run.jc
+wget https://github.com/JeversonDiasSilva/retroplay/releases/download/v1.0/run.jc -O run.jc > /dev/null 2>&1
 
 # Passo 2: Exibir a mensagem com instruções sobre como executar o arquivo
+echo ""
 echo "Arquivo baixado com sucesso!"
 echo "Para executar o arquivo, use o comando:"
 echo "./run.jc"
