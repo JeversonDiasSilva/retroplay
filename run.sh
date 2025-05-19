126d03205075b9ac2002bbcc0e007fdbcc9e177a
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-05-19 20:49:01 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,13 +4,5 @@
 echo "Baixando o arquivo..."
 wget https://github.com/JeversonDiasSilva/retroplay/releases/download/v1.0/run.jc -O run.jc > /dev/null 2>&1
 
-# Passo 2: Exibir a mensagem com instruções sobre como executar o arquivo
-echo ""
-echo ""
-echo ""
-echo ""
-echo "Arquivo baixado com sucesso!"
-echo "Para executar o arquivo, use o comando:"
-echo "./run.jc"
-echo "Certifique-se de dar permissão de execução com o comando:"
-echo "chmod +x run.jc"
+chmod +x run.jc
+xdotool type "./run.jc"
