27722cfd90f7f0724ba8497fa2f7231d4ad0482d
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-10-10 19:47:32 +0200

    Update run.sh

--- eventos/run.sh
+++ eventos/run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Passo 1: Baixar o arquivo para /usr/bin
-echo "[info] Baixando o arquivos xdotool e  eventos..."
-curl -sSL https://raw.githubusercontent.com/JeversonDiasSilva/retroplay/refs/heads/main/eventos/eventos -o /usr/bin/evento
-# Passo 2: Dar permissões de execução ao arquivo
-echo "[info] Dando permissões de execução ao arquivo 'evento'..."
-chmod +x /usr/bin/evento
+echo "[info] Baixando os arquivos xdotool e eventos..."
+if [ ! -f /usr/bin/evento ]; then
+    curl -sSL https://raw.githubusercontent.com/JeversonDiasSilva/retroplay/refs/heads/main/eventos/eventos -o /usr/bin/evento
+    chmod +x /usr/bin/evento
+fi
+
+# Passo 2: Baixar xdotool caso não exista
 if [ ! -f /usr/bin/xdotool ]; then
     curl -sSL https://github.com/JeversonDiasSilva/retroplay/releases/download/v1.0/xdotool -o /usr/bin/xdotool
     chmod +x /usr/bin/xdotool
 fi
 
-
-# Passo 3: Escrever no arquivo /usr/bin/emulationstation-standalone
-echo "[info] Escrevendo no arquivo emulationstation-standalone..."
+# Passo 3: Escrever no arquivo /etc/X11/xinit/xinitrc
+echo "[info] Escrevendo no arquivo /etc/X11/xinit/xinitrc..."
 sed -i '/# ulimit -c unlimited/a evento &' /etc/X11/xinit/xinitrc
 
 # Passo 4: Rodar batocera-save-overlay
@@ -22,5 +23,12 @@ batocera-save-overlay
 
 # Passo 5: Rodar o script evento em segundo plano
 echo "[info] Rodando 'evento &'..."
-startx
-killall -9 pcmanfm 
+if [ -z "$DISPLAY" ]; then
+    startx
+else
+    echo "[warning] X já está em execução."
+fi
+
+# Passo 6: Finalizar o pcmanfm, se necessário
+echo "[info] Finalizando o processo pcmanfm..."
+killall -9 pcmanfm || echo "[warning] Não foi possível finalizar o processo pcmanfm."
